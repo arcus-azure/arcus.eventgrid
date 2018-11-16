7e3dcf8ce509d8b02a85397a635e678f099adabf
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-11-16 12:28:24 +0100

    Introduce ConsoleLogger
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Arcus.EventGrid.Testing/Logging/ConsoleLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Arcus.EventGrid.Testing.Logging
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var message = formatter(state, exception);
+            Console.WriteLine($"{DateTimeOffset.UtcNow:s} {logLevel} > {message}");
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+    }
+}
