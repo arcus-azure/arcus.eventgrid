be4700bbc65ddad03f4738d6a4a13b75970811a0
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-05-21 14:47:24 +0200

    feat: use new Azure SDK for testing package

--- src/Arcus.EventGrid.Testing/Infrastructure/Hosts/ServiceBus/SubscriptionBehavior.cs
+++ src/Arcus.EventGrid.Testing/Infrastructure/Hosts/ServiceBus/SubscriptionBehavior.cs
@@ -1,8 +1,20 @@
 ﻿namespace Arcus.EventGrid.Testing.Infrastructure.Hosts.ServiceBus
 {
+    /// <summary>
+    /// Represents the available options in behavior when Azure Service Bus topic subscriptions are managed during the lifetime of the <see cref="ServiceBusEventConsumerHost"/>.
+    /// </summary>
     public enum SubscriptionBehavior
     {
+        /// <summary>
+        /// Keeps all the Azure Service Bus topic subscriptions that were created during the lifetime of the <see cref="ServiceBusEventConsumerHost"/>
+        /// when the consumer host stops receiving traffic.
+        /// </summary>
         KeepOnClosure,
+        
+        /// <summary>
+        /// Deletes all the Azure Service Bus topic subscriptions that were created during the lifetime of the <see cref="ServiceBusEventConsumerHost"/>
+        /// when the consumer host stops receiving traffic.
+        /// </summary>
         DeleteOnClosure
     }
 }
