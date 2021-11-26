c5a62d1b042699f92631ddc3787c3a173bbfb8ed
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-11-26 14:24:18 +0100

    pr-fix: update with correct project reference

--- src/Arcus.EventGrid.Security.WebApi/EventGridSubscriptionValidationActionFilter.cs
+++ src/Arcus.EventGrid.Security.WebApi/EventGridSubscriptionValidationActionFilter.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Primitives;
 
-namespace Arcus.EventGrid.WebApi.Security
+namespace Arcus.EventGrid.Security.WebApi
 {
     /// <summary>
     /// Represents a filter to validate Azure Event Grid subscription events.
