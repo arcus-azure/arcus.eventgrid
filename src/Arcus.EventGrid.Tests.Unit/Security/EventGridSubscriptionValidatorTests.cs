3a212824890641efe0134768fa4fd57b1f6d63c1
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-11-26 14:00:46 +0100

    feat: add eventgrid subscription to az functions

---
+++ src/Arcus.EventGrid.Tests.Unit/Security/EventGridSubscriptionValidatorTests.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arcus.EventGrid.Contracts;
+using Arcus.EventGrid.Parsers;
+using Arcus.EventGrid.Security.Core.Validation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Arcus.EventGrid.Tests.Unit.Security
+{
+    public class EventGridSubscriptionValidatorTests
+    {
+        private static readonly ILogger<EventGridSubscriptionValidator> NullLogger = NullLogger<EventGridSubscriptionValidator>.Instance;
+
+        [Fact]
+        public void ValidateCloudEventsHandshakeRequest_WithWebRequestOriginHeaderInHttpRequest_Succeeds()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+            var expectedRequestOrigin = $"request-origin-{Guid.NewGuid()}";
+            context.Request.Headers.Add("WebHook-Request-Origin", expectedRequestOrigin);
+
+            // Act
+            IActionResult result = validator.ValidateCloudEventsHandshakeRequest(context.Request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkResult>(result);
+            Assert.Contains(new StringValues(expectedRequestOrigin), context.Response.Headers["WebHook-Allowed-Origin"]);
+        }
+
+        [Fact]
+        public void ValidateCloudEventsHandshakeRequest_WithHttpRequestWithoutProperHeader_Fails()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+
+            // Act
+            IActionResult result = validator.ValidateCloudEventsHandshakeRequest(context.Request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ValidateEventGridSubscriptionRequest_WithSubscritionValidationCodeInHttpRequestBody_Succeeds()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+            var validationCode = "512d38b6-c7b8-40c8-89fe-f46f9e9622b6";
+            var validationEvent = new EventGridEvent<SubscriptionValidationEventData>(
+                id: "2d1781af-3a4c-4d7c-bd0c-e34b19da4e66",
+                subject: "Sample.Event",
+                data: new SubscriptionValidationEventData(validationCode),
+                dataVersion: "1",
+                eventType: "Microsoft.EventGrid.SubscriptionValidationEvent");
+            string json = JsonConvert.SerializeObject(validationEvent);
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(json));
+
+            // Act
+            IActionResult result = await validator.ValidateEventGridSubscriptionEventRequestAsync(context.Request);
+
+            // Assert
+            Assert.NotNull(result);
+            var responseBody = Assert.IsType<OkObjectResult>(result).Value;
+            var subscriptionValidationResponse = Assert.IsType<SubscriptionValidationResponse>(responseBody);
+            Assert.Equal(validationCode, subscriptionValidationResponse.ValidationResponse);
+        }
+
+        [Fact]
+        public async Task ValidateEventGridSubscriptionRequest_WithoutSubscritionValidationCodeInHttpRequestBody_Fails()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+            var validationEvent = new EventGridEvent<SubscriptionValidationEventData>(
+                id: "2d1781af-3a4c-4d7c-bd0c-e34b19da4e66",
+                subject: "Sample.Event",
+                data: new SubscriptionValidationEventData(validationCode: null),
+                dataVersion: "1",
+                eventType: "Microsoft.EventGrid.SubscriptionValidationEvent");
+            string json = JsonConvert.SerializeObject(validationEvent);
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(json));
+
+            // Act
+            IActionResult result = await validator.ValidateEventGridSubscriptionEventRequestAsync(context.Request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ValidateEventGridSubscriptionRequest_WithHttpRequestWithoutRequestBody_Fails()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+
+            // Act
+            IActionResult result = await validator.ValidateEventGridSubscriptionEventRequestAsync(context.Request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("    ")]
+        public async Task ValidateEventGridSubscriptionRequest_WithBlankHttpRequestBody_Fails(string requestBody)
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(requestBody));
+
+            // Act
+            IActionResult result = await validator.ValidateEventGridSubscriptionEventRequestAsync(context.Request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ValidateEventGridSubscriptionRequest_WithMultipleEventsInHttpRequestBody_Fails()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+            EventBatch<Event> blobCreatedEvent = EventParser.Parse(Artifacts.EventSamples.BlobCreateEvent);
+            EventBatch<Event> iotDeviceDeletedEvent = EventParser.Parse(Artifacts.EventSamples.IoTDeviceDeleteEvent);
+            IEnumerable<Event> events = blobCreatedEvent.Events.Concat(iotDeviceDeletedEvent.Events);
+            string requestBody = JsonConvert.SerializeObject(events);
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(requestBody));
+
+            // Act
+            IActionResult result = await validator.ValidateEventGridSubscriptionEventRequestAsync(context.Request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ValidateEventGridSubscriptionRequest_WithWrongEventTypeInHttpRequestBody_Fails()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(Artifacts.EventSamples.BlobCreateEvent));
+
+            // Act
+            IActionResult result = await validator.ValidateEventGridSubscriptionEventRequestAsync(context.Request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void ValidateCloudEventsHandshakeRequest_WithoutHttpRequest_Fails()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+            var context = new DefaultHttpContext();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => validator.ValidateCloudEventsHandshakeRequest(request: null));
+        }
+
+        [Fact]
+        public void ValidateEventGridSubscriptionRequest_WithoutHttpRequest_Fails()
+        {
+            // Arrange
+            var validator = new EventGridSubscriptionValidator(NullLogger);
+
+            // Act / Assert
+            Assert.ThrowsAnyAsync<ArgumentException>(
+                () => validator.ValidateEventGridSubscriptionEventRequestAsync(request: null));
+        }
+    }
+}
---
+++ src/Arcus.EventGrid.Tests.Unit/Security/Extensions/IServiceCollectionExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Arcus.EventGrid.Security.Core.Validation;
+using Arcus.EventGrid.Tests.Unit.Security.Fixture;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Arcus.EventGrid.Tests.Unit.Security.Extensions
+{
+    public class IServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddEventGridSubscriptionValidation_WithServices_RegistersInstance()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddEventGridSubscriptionValidation();
+
+            // Assert
+            IServiceProvider provider = services.BuildServiceProvider();
+            Assert.NotNull(provider.GetService<IEventGridSubscriptionValidator>());
+        }
+
+        [Fact]
+        public void AddEventGridSubscriptionValidation_WithFuntionHostServices_RegisteresInstance()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var builder = new StubFunctionsHostBuilder(services);
+
+            // Act
+            builder.AddEventGridSubscriptionValidation();
+
+            // Assert
+            IServiceProvider provider = builder.Services.BuildServiceProvider();
+            Assert.NotNull(provider.GetService<IEventGridSubscriptionValidator>());
+        }
+    }
+}
